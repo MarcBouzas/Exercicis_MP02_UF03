e03403522345882ccac5fb78a5dd671f8f154ff8
MarcBouzas <73602989+MarcBouzas@users.noreply.github.com>
2021-02-12 17:44:19 +0100

    Add files via upload

---
+++ sp07_sumaSousDepartament.sql
@@ -0,0 +1,21 @@
+
+USE empresa;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS sp07_sumaSousDepartament//
+
+CREATE PROCEDURE sp07_sumaSousDepartament(
+     IN  pi_num_dept smallint, 
+     OUT po_suma float)
+
+  BEGIN
+
+    SELECT SUM(SOU_TREB)
+      INTO @po_suma
+    FROM   TREBALLADORS
+    WHERE  CODI_DEP_TREB=pi_num_dept;
+
+  END //
+
+DELIMITER ;
