77a858adbe87435f71ad7331cf0f885db1ade39d
MarcBouzas <73602989+MarcBouzas@users.noreply.github.com>
2021-04-28 19:41:44 +0200

    Add files via upload

---
+++ (M02)UF3. ActivitatsCursors/BouzasMarc_Act_03_ProcEmm_MySQL_Apartat_014.sql
@@ -0,0 +1,26 @@
+USE videoclub;
+DELIMITER //
+DROP TRIGGER IF EXISTS act14_trigger_1;
+DROP TRIGGER IF EXISTS act14_trigger_2;
+
+CREATE TRIGGER act14_trigger_1 
+	BEFORE INSERT ON PELLICULES
+	FOR EACH ROW 
+		BEGIN 
+			SET NEW.titol_peli = upper(new.titol_peli);
+		END//
+CREATE TRIGGER act14_trigger_2 
+	BEFORE UPDATE ON PELLICULES
+	FOR EACH ROW 
+		BEGIN 
+			SET NEW.titol_peli = upper(new.titol_peli);
+		END//
+        
+DELIMITER ;
+SELECT id_peli, titol_peli
+FROM PELLICULES
+WHERE id_peli = 1;
+
+UPDATE PELLICULES
+SET any_peli=2003
+WHERE id_peli = 2;
