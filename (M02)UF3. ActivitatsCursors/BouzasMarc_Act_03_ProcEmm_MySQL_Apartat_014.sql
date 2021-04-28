4682722c8d62b8ce064a69006458fa0585788fbc
MarcBouzas <73602989+MarcBouzas@users.noreply.github.com>
2021-04-28 22:06:19 +0200

    Add files via upload

--- (M02)UF3. ActivitatsCursors/BouzasMarc_Act_03_ProcEmm_MySQL_Apartat_014.sql
+++ (M02)UF3. ActivitatsCursors/BouzasMarc_Act_03_ProcEmm_MySQL_Apartat_014.sql
@@ -4,17 +4,17 @@ DROP TRIGGER IF EXISTS act14_trigger_1;
 DROP TRIGGER IF EXISTS act14_trigger_2;
 
 CREATE TRIGGER act14_trigger_1 
-	BEFORE INSERT ON PELLICULES
-	FOR EACH ROW 
-		BEGIN 
-			SET NEW.titol_peli = upper(new.titol_peli);
-		END//
+  BEFORE INSERT ON PELLICULES
+    FOR EACH ROW 
+      BEGIN 
+        SET NEW.titol_peli = upper(new.titol_peli);
+          END//
 CREATE TRIGGER act14_trigger_2 
-	BEFORE UPDATE ON PELLICULES
-	FOR EACH ROW 
-		BEGIN 
-			SET NEW.titol_peli = upper(new.titol_peli);
-		END//
+  BEFORE UPDATE ON PELLICULES
+    FOR EACH ROW 
+      BEGIN 
+        SET NEW.titol_peli = upper(new.titol_peli);
+          END//	
         
 DELIMITER ;
 SELECT id_peli, titol_peli
